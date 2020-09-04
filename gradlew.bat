f81058f689898d7154fb908f45fa0ecd11284ec8
Gabriel Viana <aqua.camus@hotmail.com>
2020-09-03 23:49:53 -0300

    autalizei

--- gradlew.bat
+++ gradlew.bat
@@ -1,3 +1,27 @@
+@rem ***************************************************************************
+@rem
+@rem MIT License
+@rem
+@rem Copyright (c) 2020 Softblue
+@rem
+@rem Permission is hereby granted, free of charge, to any person obtaining a copy
+@rem of this software and associated documentation files (the "Software"), to deal
+@rem in the Software without restriction, including without limitation the rights
+@rem to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+@rem copies of the Software, and to permit persons to whom the Software is
+@rem furnished to do so, subject to the following conditions:
+@rem
+@rem The above copyright notice and this permission notice shall be included in all
+@rem copies or substantial portions of the Software.
+@rem
+@rem THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+@rem IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+@rem FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+@rem AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+@rem LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+@rem OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+@rem SOFTWARE.
+@rem ***************************************************************************
 @rem
 @rem Copyright 2015 the original author or authors.
 @rem
@@ -28,9 +52,6 @@ set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
-
-@rem Resolve any "." and ".." in APP_HOME to make it shorter.
-for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
@@ -83,7 +104,6 @@ set CMD_LINE_ARGS=%*
 @rem Setup the command line
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
 
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
